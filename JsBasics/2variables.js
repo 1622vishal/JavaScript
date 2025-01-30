3419e90ae969edafe1412915bcf330280794ae6b
Vishal Yadav <93920485+1622vishal@users.noreply.github.com>
2025-01-30 17:17:33 +0000

    Added second file

---
+++ JsBasics/2variables.js
@@ -0,0 +1,12 @@
+const num = 123    //You cannot change const variable
+let email = "test@gmail.com"
+var num2 = "hello"      //var is not used nowadays
+yourName = "John"
+
+let yourCourse;      //default value is undefined
+
+// num = 234   //not allowed
+
+console.log(num);     //semicolon doesn't matter in Js 
+
+console.table([num , email , num2 , yourName , yourCourse])
