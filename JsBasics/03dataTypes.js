d5a10bbca053f955cfa182b81985a4832e4dac22
Vishal Yadav <93920485+1622vishal@users.noreply.github.com>
2025-02-11 16:32:49 +0000

    Added dataTypes file

---
+++ JsBasics/03dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"    //we use this to define standard that we are using newer version of JS(treat older code as newer)
+
+// alert(3 + 3)    //cannot use alert in node as this runs alert gives popup in browser
+
+let name = "Vishal" //string data type
+let age = 22    //number type
+let isMale = true   //boolean
+let location = null //null represents nothing
+
+console.log(typeof age) //type of tells the type of data type
+
+/*let's see what's the type of null*/
+console.log(typeof(null))   //you can write in this way also
